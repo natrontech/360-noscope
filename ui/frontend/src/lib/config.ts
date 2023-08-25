0ec30dbd581c614065f15d23b39c30f100792dad
Jan Lauber <jan.lauber@protonmail.ch>
2023-08-25 23:01:33 +0200

    feat: add ui skeleton
    Signed-off-by: Jan Lauber <jan.lauber@protonmail.ch>

---
+++ ui/frontend/src/lib/config.ts
@@ -0,0 +1,5 @@
+export const site = {
+    name: "360-noscope",
+    description:
+        "To support federal communities in analyzing their eco development, we have developed an analysis tool called the 360° Scan."
+};
---
+++ ui/frontend/src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import "../app.postcss";
+  import "../app.css";
+  import { metadata } from "$lib/stores/metadata";
+  import { site } from "$lib/config";
+  import { beforeNavigate } from "$app/navigation";
+  import { Toaster } from "svelte-french-toast";
+  
+  $: title = $metadata.title ? $metadata.title + " | " + site.name : site.name;
+  $: description = $metadata.description ?? site.description;
+
+  // reset metadata on navigation so that the new page inherits nothing from the old page
+  beforeNavigate(() => {
+    $metadata = {};
+  });
+
+</script>
+
+<svelte:head>
+  <title>{title}</title>
+  <meta name="description" content={description} />
+</svelte:head>
+
+<div>
+  <Toaster position="bottom-center" />
+  <!-- only display nav when not on /login -->
+  <slot />
+</div>
