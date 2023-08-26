e7fd38567a941ad2ee0fce22ae0548d3407135e2
Philip Jonah Kunz <philip.j.kunz@gmail.com>
2023-08-26 10:30:49 +0200

    feat: Updated ScaleController
    Signed-off-by: Philip Kunz <philip.j.kunz@gmail.com>

--- analyze/src/main/java/io/natron/noscope360/analyze/controller/ScaleController.java
+++ analyze/src/main/java/io/natron/noscope360/analyze/controller/ScaleController.java
@@ -5,6 +5,8 @@ import io.natron.noscope360.analyze.model.enums.Theme;
 import io.natron.noscope360.analyze.model.indicator.Indicator;
 import io.natron.noscope360.analyze.service.ScaleService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -42,11 +44,16 @@ public class ScaleController {
      *
      * @return List of available dimensions.
      */
-    @Operation(summary = "Get all dimension.", security = @SecurityRequirement(name = "basicAuth"))
+    @Operation(summary = "Retrieve all available dimensions.", security = @SecurityRequirement(name = "basicAuth"))
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved list of dimensions"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized - Invalid authentication credentials"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     @GetMapping(path = "/dimensions", produces = APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
     public List<Dimension> getDimensions() {
-        log.info("Get all dimension.");
+        log.info("Retrieving all dimensions.");
         return scaleService.getDimensions();
     }
 
@@ -56,11 +63,17 @@ public class ScaleController {
      * @param dimension The dimension for which themes are to be retrieved.
      * @return List of themes associated with the specified dimension.
      */
-    @Operation(summary = "Get all themes of an dimension.", security = @SecurityRequirement(name = "basicAuth"))
-    @GetMapping(path = "/{dimension}/themes", produces = APPLICATION_JSON_VALUE)
+    @Operation(summary = "Retrieve all themes associated with a given dimension.", security = @SecurityRequirement(name = "basicAuth"))
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved list of themes for the given dimension"),
+            @ApiResponse(responseCode = "400", description = "Bad Request - Invalid dimension specified"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized - Invalid authentication credentials"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    @GetMapping(path = "/dimensions/{dimension}/themes", produces = APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
     public List<Theme> getThemes(@PathVariable String dimension) {
-        log.info("Get all themes of an dimension.");
+        log.info("Retrieving all themes of a given dimension.");
         return scaleService.getThemes(dimension);
     }
 
@@ -71,11 +84,17 @@ public class ScaleController {
      * @param theme     The theme for which indicators are to be retrieved.
      * @return List of indicators associated with the specified theme.
      */
-    @Operation(summary = "Get all indicators of a theme.", security = @SecurityRequirement(name = "basicAuth"))
-    @GetMapping(path = "/{dimension}/{theme}/", produces = APPLICATION_JSON_VALUE)
+    @Operation(summary = "Retrieve all indicators associated with a specific theme within a dimension.", security = @SecurityRequirement(name = "basicAuth"))
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved list of indicators for the given theme within a dimension"),
+            @ApiResponse(responseCode = "400", description = "Bad Request - Invalid dimension or theme specified"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized - Invalid authentication credentials"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    @GetMapping(path = "/dimensions/{dimension}/themes/{theme}/indicators", produces = APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
     public List<Indicator> getIndicators(@PathVariable String dimension, @PathVariable String theme) {
-        log.info("Get all indicators of a theme.");
+        log.info("Retrieving all indicators for a specified theme.");
         return scaleService.getIndicatorsByTheme(dimension, theme);
     }
 
@@ -85,11 +104,17 @@ public class ScaleController {
      * @param municipality The municipality for which the scales of the indicators are to be retrieved.
      * @return List of the scales of the indicators associated with the specified municipality.
      */
-    @Operation(summary = "Get all indicators of a specific municipality.", security = @SecurityRequirement(name = "basicAuth"))
-    @GetMapping(path = "/indicator", produces = APPLICATION_JSON_VALUE)
+    @Operation(summary = "Retrieve all indicators associated with a specific municipality.", security = @SecurityRequirement(name = "basicAuth"))
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved list of indicators for the specified municipality"),
+            @ApiResponse(responseCode = "400", description = "Bad Request - Invalid municipality specified"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized - Invalid authentication credentials"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    @GetMapping(path = "/indicators", produces = APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
     public List<Indicator> getIndicators(@RequestParam String municipality) {
-        log.info("Get all indicators of a specific municipality.");
+        log.info("Retrieving all indicators for a specific municipality.");
         return scaleService.getIndicatorsByMunicipality(municipality);
     }
 }
