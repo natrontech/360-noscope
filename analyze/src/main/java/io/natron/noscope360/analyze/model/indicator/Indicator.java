57f8426053663bc9968ee54d63f347ac65d92430
Joel-Haeberli <haebu@rubigen.ch>
2023-08-26 02:24:42 +0200

    feat: indicator base implementation

--- analyze/src/main/java/io/natron/noscope360/analyze/model/indicator/Indicator.java
+++ analyze/src/main/java/io/natron/noscope360/analyze/model/indicator/Indicator.java
@@ -1,4 +1,26 @@
 package io.natron.noscope360.analyze.model.indicator;
 
-public abstract class Indicator {
+import java.time.LocalDate;
+import java.util.Map;
+import java.util.function.Function;
+
+
+public abstract class Indicator<T> {
+
+    protected String category; // TODO replace with enum
+    protected String theme; // TODO replace with enum
+    protected String name;
+    protected LocalDate date;
+
+    protected Map<String, T> values;
+
+    protected Map<String, Function<T, Number>> mappings;
+
+    public Number calculateScale() {
+
+        return values.keySet().stream()
+                .mapToDouble(k -> mappings.get(k).apply(values.get(k)).doubleValue())
+                .average()
+                .orElseThrow(IllegalStateException::new);
+    }
 }
